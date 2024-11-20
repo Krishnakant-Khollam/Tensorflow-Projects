e5ebb1be312bc14cbf700dc2e8b2f7b5454c043c
Krishnakant-Khollam <141311610+Krishnakant-Khollam@users.noreply.github.com>
2024-11-20 15:16:35 +0530

    Add files via upload

---
+++ OBJECT DETECTION/Object Detection.py
@@ -0,0 +1,87 @@
+import numpy as np
+import os
+import cv2
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+import tensorflow as tf
+
+cls = lambda: os.system("cls")
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+configs = config_util.get_configs_from_pipeline_file(r"models\pipeline.config")
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore('models\myssdmobnet\ckpt-3').expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+category_index = label_map_util.create_category_index_from_labelmap(r"models\label_map.pbtxt")
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    image_np = np.array(frame)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=.8,
+                agnostic_mode=False)
+
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+# command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'])
+
+#train
+# python models\models-master\research\object_detection\model_main_tf2.py --model_dir=models\myssdmobnet --pipeline_config_path=models\pipeline.config --num_train_steps=2000
+
+#evaluate
+"""command = "python {} --model_dir={} --pipeline_config_path={} --checkpoint_dir={}".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'],files['PIPELINE_CONFIG'], paths['CHECKPOINT_PATH'])"""
+
+""" python models\models-master\research\object_detection\model_main_tf2.py --model_dir=models\myssdmobnet --pipeline_config_path=models\pipeline.config --checkpoint_dir=models\myssdmobnet
+"""
+
+
+# generate .record files
+
+"""python {files['TF_RECORD_SCRIPT']} -x {os.path.join(paths['IMAGE_PATH'], 'train')} -l {files['LABELMAP']} -o {os.path.join(paths['ANNOTATION_PATH'], 'train.record')}"""
+
+
+"""python models\generate_tfrecord.py -x images\train -l models\label_map.pbtxt -o models\train.record"""
+
+
+
+
+
